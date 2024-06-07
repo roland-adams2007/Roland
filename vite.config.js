5a47c91aabfb675fb2669d25196e49d498791083
roland-adams2007 <adamsrolly7@gmail.com>
2024-06-07 01:06:28 +0100

    again

--- vite.config.js
+++ vite.config.js
@@ -11,13 +11,27 @@
 // })
 
 // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   base: '/Roland', // Set your GitHub repository name here
+//   plugins: [react()],
+//   root: 'src',
+//   build: {
+//     outDir: '../dist', // Specify your desired output directory
+//   },
+// });
+
+
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/Roland', // Set your GitHub repository name here
   plugins: [react()],
-  root: 'src',
+  root: '.', // Specify your project root (source directory)
   build: {
     outDir: '../dist', // Specify your desired output directory
   },
